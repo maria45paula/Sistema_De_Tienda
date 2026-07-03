f2f305f25be2ab0dc6468d5fd5a3b7c6ccc64757
maria45paula <paulaugggvbk@gmail.com>
2026-07-02 21:06:11 -0500

    Modificaciones en la clase

--- src/main/java/org/taller/Pedido.java
+++ src/main/java/org/taller/Pedido.java
@@ -1,47 +1,64 @@
 package org.taller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import static org.taller.Producto.getListaProductos;
 import static org.taller.Producto.getNombreProductos;
+import static org.taller.Respuesta.SI;
+
 
 public class Pedido {
-    private static List<Producto> pedidos;
-    private List<Pedido> factura;
+    private List<Producto> pedidos = new ArrayList<>();
+    private List<Pedido> pedidosPendientes = new ArrayList<>();
+    private MetodosPago metodoDePago;
+    private String estadoDePedido;
 
-    public Pedido(Producto producto) {
-        this.pedidos.add(producto);
+    public Pedido(List<Producto> pedidos) {
+        this.pedidos = pedidos;
     }
 
-    public void agregarProductoAFactura(Respuesta respuestaUsuario) {
+    public void agregarProductoAFactura() {
+        Respuesta respuestaUsuario = SI;
         Scanner entrada = new Scanner(System.in);
-        int posicion = 0;
-
-        if (respuestaUsuario == Respuesta.SI) {
-            System.out.println("\n--- Lista de Productos ---");
-            for (Producto prod : getListaProductos()) {
-                System.out.println("Producto: " + prod.getNombre() + " | Precio: $" + prod.getPrecio());
-            }
-            System.out.println("Ingrese el producto a añadir en el pedido:");
-            String producto = entrada.nextLine();
-
-            for (String nombre : getNombreProductos()) {
-
-                if (producto.equalsIgnoreCase(nombre)) {
-                    pedidos.add(getListaProductos().get(posicion));
+        int posicion;
+        do {
+            posicion = 0;
+            if (respuestaUsuario == SI) {
+                System.out.println("\n--- Lista de Productos ---");
+                for (Producto prod : getListaProductos()) {
+                    System.out.println("Producto: " + prod.getNombre() + " | Precio: $" + prod.getPrecio());
                 }
-                posicion++;
+                System.out.println("Ingrese el producto a añadir en el pedido:");
+                String producto = entrada.nextLine();
+
+                for (String nombre : getNombreProductos()) {
+
+                    if (producto.equalsIgnoreCase(nombre)) {
+                        pedidos.add(getListaProductos().get(posicion));
+                    }
+                    posicion++;
+                }
             }
-        } else if (respuestaUsuario == Respuesta.NO) {
-            finalizarCompra();
-        }
+            System.out.println("¿Desea ingresar otro producto a su pedido?");
+            respuestaUsuario = Respuesta.valueOf(entrada.nextLine());
+        } while (respuestaUsuario == Respuesta.SI);
+        finalizarCompra();
     }
 
-    public static void finalizarCompra() {
+    public void finalizarCompra() {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("\n--- Factura---");
         for (Producto factura : pedidos) {
             System.out.println("Producto:" + factura.getNombre() + " Precio:$" + factura.getPrecio());
         }
+
+        System.out.println("Elija el metodo de pago:");
+        metodoDePago = MetodosPago.valueOf(entrada.nextLine());
+
+        Pedido pedido = new Pedido(new ArrayList<>(pedidos));
+        pedidosPendientes.add(pedido);
+
     }
 }
